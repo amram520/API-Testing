872b8dd44ac14e873e2f595df5784bede4e53cf6
daniel <amram520@gmail.com>
2024-08-29 16:27:23 +0300

    update

--- src/test/java/e2e/tests/BaseTest.java
+++ src/test/java/e2e/tests/BaseTest.java
@@ -36,19 +36,21 @@ public class BaseTest extends TestCase {
 
     @SneakyThrows
     @AfterMethod
-    public void closeBrowser() {
-//        String path = System.getProperty("user.dir");
-//        String testName = result.getMethod().getMethodName();
-//        Path videoName = page.video().path().getFileName();
+    public void closeBrowser(ITestResult result) {
+        String path = System.getProperty("user.dir");
+        String testName = result.getMethod().getMethodName();
+        Path videoName =  page.video().path().getFileName();
         page.close();
         context.close();
         browser.close();
-//        Path isFileExist = Paths.get("").toAbsolutePath().resolve("videos\\" + testName + ".webm");
-//        if (Files.exists(isFileExist)) {
-//            Files.delete(isFileExist);
+        Path isFileExist = Paths.get("").toAbsolutePath().resolve("videos\\"+testName+".webm");
+        if(Files.exists(isFileExist)){
+            Files.delete(isFileExist);
+        }
+        File file1 = new File(path+File.separator+"videos"+File.separator+videoName);
+        File file2 = new File(path+File.separator+"videos"+File.separator+testName+".webm");
+        file1.renameTo(file2);
             playwright.close();
-
-//        }
-
+        }
     }
-}
+
